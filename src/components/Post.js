bd8f4000196fc84e2571646502ff604aaadc227f
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-14 20:36:01 +0700

    adding post page detail

---
+++ src/components/Post.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class Post extends Component {
+    state  = {
+        post: []
+    }
+    componentDidMount() {
+        let id = this.props.match.params.post_id
+        axios.get('http://jsonplaceholder.typicode.com/posts/' + id)
+        .then(res =>{
+            this.setState({
+                post: res.data
+            })
+        })
+    }
+    render() {
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{this.state.post.title}</h4>
+                <p>{this.state.post.body}</p>
+            </div>
+        ) : (
+            <div className="center">
+                Loading post....
+            </div>
+        )
+        return (
+            <div className="container">
+                {post}
+            </div>
+        )
+    }
+}
+
+export default Post
