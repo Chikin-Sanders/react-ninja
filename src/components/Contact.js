372db78540175c1034070388c362c83f576cf379
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-14 20:35:14 +0700

    add contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima provident dolore voluptate earum, sed tenetur natus animi sapiente, debitis corrupti, nam magnam? Debitis enim odio rem eum, molestiae repudiandae rerum.</p>
+            
+        </div>
+    )
+}
+
+export default Contact
