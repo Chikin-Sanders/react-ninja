1b2cfc997220a27f6c33268bd078e74acf7227b8
Aziz Zakawali <zakaziz86@gmail.com>
2021-01-14 20:35:03 +0700

    add About page

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+function About() {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, possimus dolores itaque in ipsum distinctio, sit hic delectus culpa molestias unde, quas eum dolorum cupiditate eaque ipsam neque odit aut?</p>
+            
+        </div>
+    )
+}
+
+export default Rainbow(About)
